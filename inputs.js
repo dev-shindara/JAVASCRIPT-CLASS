092e8a64ece727357e8066aa03541e2ba0258ce8
Jeremiah Olisa <jeremiaholisa453@gmqil.com>
2022-01-22 16:25:04 +0100

    input updATED

--- inputs.js
+++ inputs.js
@@ -1,4 +1,9 @@
-const prompt = require('prompt-sync');
-const input = prompt({ sigint: true })
+// const prompt = require('prompt-sync');
+// const input = prompt({ sigint: true })
+
+const input = require('prompt-sync')({ sigint: true });
 
 let name = input("What is your name: ")
+
+
+console.log("My name is " + name)
