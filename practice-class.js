1695cffc757828aa6f9bd1347651747bed90c5e5
Jeremiah Olisa <jeremiaholisa453@gmqil.com>
2022-01-22 17:47:35 +0100

    ✨ spackles

--- practice-class.js
+++ practice-class.js
@@ -1,5 +1,5 @@
 const input = require("prompt-sync")({ sigint: true });
-console.log("WELCOME TO SHINDARA DRIVING LICENSE ISSUING APPLICATION");
+console.log("WELCOME TO SHINDARA DRIVING LICENSE ISSUING APPLICATION ✨✨✨");
 // console.log("shindara omoloye")
 let fullname = input("what is your fullname? ");
 let age = Math.round(parseInt(input("how old are you? ")));
