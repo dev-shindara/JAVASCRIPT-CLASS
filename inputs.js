2dc8e828e73c458e7d82539b31571772d6053db6
Jeremiah Olisa <jeremiaholisa453@gmqil.com>
2022-01-22 17:30:37 +0100

    input code updated

--- inputs.js
+++ inputs.js
@@ -7,3 +7,14 @@ let name = input("What is your name: ")
 
 
 console.log("My name is " + name)
+
+let ageString = input("how old are you")
+let ageNumber = parseInt(ageString)
+let ageRoundedUp = Math.round(ageNumber)
+
+// print out "Hello " + name
+if(ageRoundedUp < 18){
+    // print out "You are not eligible for a DRIVING license"
+}else{
+    // print out "You are eligible for a DRIVING license, HURRAY!!!"
+}
